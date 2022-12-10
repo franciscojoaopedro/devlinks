a2aebb406b19af64a80a6a44f5143e5ac27955c3
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2022-12-10 09:32:52 +0000

    add page , links and get links

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,33 +1,63 @@
+import { useState,useEffect } from "react";
 import { Social } from "../../components/Social";
 import "./home.css"
 import {FaFacebook,FaLinkedinIn,FaYoutube} from "react-icons/fa"
+import {
+getDocs,
+collection,
+orderBy,
+query
+} from "firebase/firestore"
+import {db} from "../../services/firebaseConnection"
+
 
 const Home=()=>{
+
+    const [links,setLinks]=useState([])
+
+useEffect(()=>{
+    async function loadLinks(){
+        const linksRef=collection(db,"links")
+        const queryRef=query(linksRef,orderBy("created","asc"))
+        getDocs(queryRef)
+        .then((snapshot)=>{
+            let lista=[]
+
+            snapshot.forEach((doc)=>{
+                lista.push({
+                    id:doc.id,
+                    name:doc.data().name,
+                    url:doc.data().url,
+                    bg:doc.data().bg,
+                    color:doc.data().color,
+                })
+            })
+            setLinks(lista)
+
+        })
+    }
+    loadLinks()
+},[])
+
+useEffect(()=>{
+
+},[])
+
     return(
         <div className="home-container">
             <h1>Francisco João Pedro</h1>
             <span>Veja os meus links👇</span>
            <main className="links">
-           <section className="link-area">
-                <a href="#">
-                    <p className="link-text">Nosso Canal no Youtube</p>
+           {
+            links.map((item,index)=>(
+                <section key={index} className="link-area" style={{backgroundColor:item.bg}}>
+                <a href={item.url} target="_blank">
+                    <p className="link-text" style={{color:item.color}} >{item.name}</p>
                 </a>
             </section>
-            <section className="link-area">
-                <a href="https://www.facebook.com/ngunga.pedro.1/" target={"_blank"}>
-                    <p className="link-text">Meu Facebook</p>
-                </a>
-            </section>
-            <section className="link-area">
-                <a href="https://github.com/franciscojoaopedro" target={"_blank"}>
-                    <p className="link-text">Meu GitHub</p>
-                </a>
-            </section>
-            <section className="link-area">
-                <a href="https://franciscojoaopedro-dev.vercel.app/" target={"_blank"}>
-                    <p className="link-text">Meu Portifólio</p>
-                </a>
-            </section>
+            ))
+           }
+            
 
             <footer>
                 <Social url="https://www.facebook.com/ngunga.pedro.1/" >
